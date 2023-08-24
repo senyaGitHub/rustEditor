8b2199e97519af80b6e4a5f17ffd36d9bdfbe0c8
senyaGitHub <knight.fo@yandex.ru>
2023-08-24 13:46:50 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+
+fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
+}
